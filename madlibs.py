3228594ff8590596108b73f14e95158b9d356fe4
JovanD <58370468+DubovacJ@users.noreply.github.com>
2023-12-13 14:16:58 +0100

    branch applied
    applied to the branch random code

---
+++ madlibs.py
@@ -0,0 +1,11 @@
+youtuber="baka prase"
+print(f'subscribe to {youtuber}')
+
+adj=input("adjective:")
+verb1=input("verb1")
+verb2= input("verb2")
+famous=input("famous person")
+madlib=f"computer programming is so {adj}! it makes me so excited all time because\
+i love to {verb1}.Stay hydrated and {verb2} like you are {famous}"
+
+print(madlib)
